63c4035d3971f5111c777ef8dff2c2c55860249d
Mike Morton <mmorton7295@gmail.com>
2021-08-08 14:46:24 -0500

    adds unit tests for post creation

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -22,9 +22,58 @@ RSpec.describe "Posts", type: :request do
       )
       get '/posts'
       posts = JSON.parse(response.body)
-      p posts
+      # p posts
       expect(posts.length).to eq(2)
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /posts" do
+    it "creates a new post in db" do
+      user = User.create!(
+        name: 'Mike',
+        email: 'mike@website.com',
+        password: 'password'
+      )
+      jwt = JWT.encode(
+        {
+          user: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        "random", # the secret key
+        'HS256' # the encryption algorithm
+      )
+      post '/posts', params: {title: 'post creation test', body: 'some test body text', image: 'image.com2'}, headers: {"Authorization" => "Bearer #{jwt}"}
+      expect(response).to have_http_status(200)
+    end
+
+    it "creates a new post in db, invalid params" do
+      user = User.create!(
+        name: 'Mike',
+        email: 'mike@website.com',
+        password: 'password'
+      )
+      jwt = JWT.encode(
+        {
+          user: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        "random", # the secret key
+        'HS256' # the encryption algorithm
+      )
+      post '/posts', params: {body: 'some test body text', image: 'image.com2'}, headers: {"Authorization" => "Bearer #{jwt}"}
+      expect(response).to have_http_status(400)
+    end
+
+    it "creates a new post in db, no jwt" do
+      user = User.create!(
+        name: 'Mike',
+        email: 'mike@website.com',
+        password: 'password'
+      )
+      post '/posts', params: {body: 'some test body text', image: 'image.com2'}
+      expect(response).to have_http_status(401)
+    end
+  end
+  
 end
